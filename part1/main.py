218dd045efe13a1c8116af423299884db00f2bd3
toddnguyen47 <todd.nguyen47@gmail.com>
2018-09-19 15:48:49 -0700

    Made a separate function for user input

--- part1/main.py
+++ part1/main.py
@@ -1,5 +1,4 @@
 import os
-import pandas
 
 file_name = "specification.csv"
 
@@ -12,18 +11,29 @@ def execute():
     if not os.path.isfile(file_name):
         print("{} does not exist in this current directory.".format(file_name))
     else:
-        with open(file_name, "r") as file:
-            data = file.read()
-
-        # Split the read data into three parts based on commas
-        data = data.split(",")
-        seed = data[0]
-        max_pages = data[1]
-        domain_restriction = data[2]
-
+        seed, max_pages, domain_restriction = obtain_user_input(file_name)
         print("Seed: {}".format(seed))
         print("Max Pages: {}".format(max_pages))
         print("Domain Restriction: {}".format(domain_restriction))
+
+
+def obtain_user_input(filename):
+    """
+    Obtain the user input from the filename.
+
+    # Arguments
+    filename -> Filename to read data from.
+    """
+    with open(file_name, "r") as file:
+            data = file.read()
+
+    # Split the read data into three parts based on commas
+    data = data.split(",")
+    seed = data[0]
+    max_pages = data[1]
+    domain_restriction = data[2]
+
+    return seed, max_pages, domain_restriction
 
 
 # Main function. Will execute if this file is run from the command line
