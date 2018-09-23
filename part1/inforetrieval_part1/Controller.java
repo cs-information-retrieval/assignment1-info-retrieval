d06525e21f73428adf536e555d144ac521569caf
toddnguyen47 <todd.nguyen47@gmail.com>
2018-09-23 01:27:40 -0700

    Changed GUI to look neater

--- part1/inforetrieval_part1/Controller.java
+++ part1/inforetrieval_part1/Controller.java
@@ -1,38 +1,25 @@
 package inforetrieval_part1;
 
-import java.io.File;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ToddNguyen
- */
 public class Controller {
-    // Make it a Singleton
-    public static Controller instance;
+    // Singleton
+    private static Controller instance;
     private Controller() {};
     
     public static Controller getInstance() {
         if (instance == null) {
             instance = new Controller();
         }
-        
         return instance;
     }
     
     
     /**
-     * Main function to execute reading of file and start to web crawl information
-     * @param input - Seed, max number of pages to crawl, and domain restriction
+     * Main function to crawl
+     * @param args - The seed, max number of pages to crawl, and domain restriction
      */
-    public void execute(String[] input) {
-        for (String s : input) {
-            System.out.println(s);
-        }
+    public void execute(String[] args) {
+        //TODO
+        // Call all the other functions
+        System.out.println("You are in execute() function");
     }
 }
