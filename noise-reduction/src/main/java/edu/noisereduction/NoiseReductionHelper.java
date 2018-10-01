d69945d8fbbec36bcfbf41ae14979dde8dcff8b8
Bhargav Parekh <42684274+nambvp03@users.noreply.github.com>
2018-10-01 13:58:27 -0700

    css base noise removal approach

---
+++ noise-reduction/src/main/java/edu/noisereduction/NoiseReductionHelper.java
@@ -0,0 +1,96 @@
+package edu.noisereduction;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+
+public class NoiseReductionHelper {
+	
+	public List<String> getAdListList() {
+		List<String> adList = new ArrayList<String>();
+		BufferedReader reader = null;
+		long timeToExpire = 1 * 24 * 60 * 60 * 1000;
+		boolean refreshAdListFileFlag = false;
+		File adListFile= new File("adList.txt");
+		try {
+			if(!adListFile.exists()) {
+				refreshAdListFileFlag = true;
+				adListFile.createNewFile();
+			}
+			long adListFreshness = new Date().getTime() - adListFile.lastModified();
+			if (!refreshAdListFileFlag && adListFreshness < timeToExpire) {
+				reader = new BufferedReader(new FileReader(adListFile));
+			}
+			else {
+				refreshAdListFileFlag = true;
+				reader= new BufferedReader(new InputStreamReader(new URL("https://easylist-downloads.adblockplus.org/easylist.txt").openStream()));
+			}
+
+			String line="";
+			StringBuilder newAdList = new StringBuilder();
+			while ((line=reader.readLine())!=null){
+				if(line.startsWith("##")){
+					newAdList.append(line+"\n");
+					adList.add(line.substring(2));
+				}
+			}
+			if(refreshAdListFileFlag) {
+				FileWriter newAdListWriter = new FileWriter(adListFile, false);
+				newAdListWriter.write(newAdList.toString());
+				newAdListWriter.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			return adList;
+		}
+	}
+	
+	public String extractContentDocument(Document htmlPage, List<String> adList){
+		//Removing nav bars
+		htmlPage.select("nav, .nav, .navbar, [class*=nav], [id*=mw-panel], [class*=navigation], [id*=navigation], [class*=recirculation], [id*=recirculation]").remove();
+		//Removing headers
+		htmlPage.select("header, .header, .head, #header, #head, [id*=mw-head]").remove();
+		//Removing footers
+		htmlPage.select("footer, .footer, #footer, [class*=footer], [class*=bottom], [id*=bottom]").remove();
+		//Removing side bars
+		htmlPage.select(".sidebar, #sidebar, [class^=sidebar], aside, [class^=aside]").remove();
+		//Removing social media buttons
+		htmlPage.select("[class*=social], [id*=social], [class*=shar], [id*=share], [class*=tool], [id*=tool]").remove();
+		htmlPage.select("[class*=breadcrumb], [id*=breadcrumb]").remove();
+		htmlPage.select("[href*=facebook], [href*=twitter], [href*=youtube], [href*=pintrest]").remove();
+		//Removing iframe, embed, script, head tags
+		htmlPage.select("iframe, embed, script, head").remove();
+		//Removing subscribe bar
+		htmlPage.select("[class*=newsletter], [class*=subscribe], [id*=newsletter], [id*=subscribe]").remove();
+		//Removing comment and feed area
+		htmlPage.select("[class=post-feeds], [class=blog-pager], [id*=comment], [role*=button], [class*=button], [id*=button]").remove();
+
+		////Removing ads from available list
+		for(String t:adList){
+			try{
+				htmlPage.select(t).remove();
+			} catch(org.jsoup.select.Selector.SelectorParseException e){
+
+			}
+		}
+
+		String text="";
+		try{
+			//Get body tag text only
+			text=htmlPage.select("body").text();
+		}
+		catch(Exception e){
+			System.out.println("Exception at text extraction");
+		}
+		return(text);
+	}
+}
